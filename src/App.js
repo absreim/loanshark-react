ef6ec57b66ddd1cbc9d54526a5dc446c97f02496
Brook Li <absreim@github.com>
2019-05-12 13:54:42 -0400

    Created scaffold for React components.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Switch, Route, Redirect, Link} from 'react-router-dom'
+
 import './App.css';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1><Link to="/">Loanshark</Link></h1>
+        <nav>
+          <Link to="/all-loans">Loans</Link>
+        </nav>
       </header>
+      <main>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/all-loans" component={AllLoans} />
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Home = () => (
+    <div>
+        <h2>Welcome to Loanshark!</h2>
+        <h3>Sign up now and start exploiting your friends!</h3>
+    </div>
+)
+
+export default Home
