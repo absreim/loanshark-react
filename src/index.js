ff4c99cc999dbfa9dbbd192707b07e54348c7729
Brook Li <emcdynamicist@gmail.com>
2019-09-10 18:13:53 -0400

    Completely overhauled repo by starting new package with Apollo.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+import { ApolloProvider, Query } from 'react-apollo'
+import gql from 'graphql-tag'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cache = new InMemoryCache()
+const link = new HttpLink({
+  uri: 'http://localhost:3000/graphql'
+})
+const client = new ApolloClient(cache, link)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// TODO: verfiy query is correct
+const GET_USER = gql`
+  query GetUser {
+    me {
+      id,
+      email,
+      name
+    }
+  }
+`
+
+// TODO: make more meaningful output
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Query query={GET_USER}>
+      {({ data }) => (<p>`Welcome, ${data.name}!`</p>)}
+    </Query>
+  </ApolloProvider>,
+  document.getElementById('root')
+)
